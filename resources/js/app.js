4355d9f278eecff51c98ddc27e3c348e47ce334a
FixUpLookSharp <tixoncow95@gmail.com>
2021-05-01 19:50:34 +0300

    update v1.5

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,18 @@ require('./bootstrap');
 import Vue from 'vue';
 import store from './store'
 import router from "./router";
+
 // markdown
 import 'v-markdown-editor/dist/v-markdown-editor.css';
 import Editor from 'v-markdown-editor'
 Vue.use(Editor);
 
+//bootstrap vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 //carousel
 import { Swiper as SwiperClass, Pagination, Mousewheel, Autoplay, Navigation } from 'swiper/swiper.esm'
--- resources/js/views/posts/CreatePostComponent.vue
+++ resources/js/views/posts/CreatePostComponent.vue
@@ -1,41 +1,51 @@
 <template>
-<!--        <div class="container">-->
-<!--            <div class="row">-->
-<!--                <div class="col-md-8 col-md-offset-2">-->
-<!--                    <h1>Написать пост</h1>-->
-<!--                    <div>-->
-<!--                        <div class="form-group has-error">-->
-<!--                            <label>Заголовок</label>-->
-<!--                            <input type="text" class="form-control" name="title" />-->
-<!--                        </div>-->
-<!--                        <div class="form-group">-->
-<!--                            <label for="exampleFormControlSelect1">Example select</label>-->
-<!--                            <select class="form-control" id="exampleFormControlSelect1">-->
-<!--                                <option>1</option>-->
-<!--                            </select>-->
-<!--                        </div>-->
-<!--                        <div class="form-group">-->
-<!--                            <label for="description">Description</label>-->
-<!--                            <markdown-editor v-model="body"></markdown-editor>-->
-<!--                        </div>-->
-<!--                        <div class="form-group">-->
-<!--                            <p><span class="require">*</span> - required fields</p>-->
-<!--                        </div>-->
-<!--                        <div class="form-group">-->
-<!--                            <button type="submit" class="btn btn-primary">-->
-<!--                                Create-->
-<!--                            </button>-->
-<!--                            <button class="btn btn-success">-->
-<!--                                Cancel-->
-<!--                            </button>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-    <div class="container">
-
-    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1>Написать пост</h1>
+                    <div>
+                        <div class="form-group has-error">
+                            <label>Заголовок</label>
+                            <input type="text" class="form-control" name="title" />
+                        </div>
+                        <div class="form-group">
+                            <label>Выберите категорию</label>
+                            <select v-model="category_id" class="form-control">
+                                <option v-for="categories in getCategories" :value="categories.id" >{{ $t(categories.name) }}</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Краткое описание</label>
+                            <textarea rows="3" class="form-control" v-model="short_description" ></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Описание</label>
+                            <markdown-editor v-model="description"></markdown-editor>
+                        </div>
+                        <div class="form-group">
+                            <b-form-file
+                                v-model="photo"
+                                :state="Boolean(photo)"
+                                placeholder="Выберите файл или перетащите сюда ..."
+                                drop-placeholder="Перетащите файл сюда ..."
+                            ></b-form-file>
+                        </div>
+                        <div class="form-group">
+                            <button @click="sendPost({
+                            title: title,
+                            creator_id: creator_id,
+                            category_id: category_id,
+                            description: description,
+                            short_description: short_description,
+                            photo: photo
+                            })" type="submit" class="btn btn-success">
+                                Создать пост
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -44,18 +54,26 @@
         data: function () {
             return {
                 title: '',
-                creator_id: null,
+                creator_id: this.getAuth.id,
                 category_id: null,
                 description: '',
                 short_description: '',
-                body: '',
-                photo: '',
+                photo: null,
             }
-        }
+        },
+        computed: {
+            ...mapGetters({
+                getCategories: 'getCategories',
+                getAuth: 'getAuth',
+            })
+        },
+
 
     }
 </script>
 
 <style scoped>
-
+.mr-5 {
+    margin-right: 0 !important;
+}
 </style>
